e53e5636a41ed2d0f4b87447d9f53b6a567c9abe
MrPhil-S <pschims@gmail.com>
2024-10-20 22:44:21 -0700

    Changes price change query to only include changes since previous run.

--- scrape_site.py
+++ scrape_site.py
@@ -136,35 +136,37 @@ def get_sale_hits(discount):
 def get_new_or_changed_prices(run_id):
     cursor.execute(f"""
 WITH cte AS
-(SELECT 
-    	b.booz_id,
-    	b.booz_name,
-    	b.link,
-    	bs.price,
-    	bs.sale_price,
-    	bs.run_id,
-    	ROW_NUMBER() OVER (PARTITION BY bs.booz_id ORDER BY bs.scrape_date DESC) AS row_num
-    FROM booz_scraped bs
-    JOIN booz  b
-    ON bs.booz_id = b.booz_id
-    WHERE bs.run_id <= 31)
-SELECT
- 	current_price.booz_id,
-    current_price.booz_name,
-    current_price.link,
-    current_price.price c_price,
-    current_price.sale_price c_sale_price,
-    CAST(100-(current_price.sale_price/current_price.price * 100) AS INT) c_discount,
-    previous_price.price p_price,
-    previous_price.sale_price p_sale_price,
-    CAST(100 - (previous_price.sale_price/previous_price.price *100) AS INT) p_discount,
-    CASE WHEN current_price.sale_price >= previous_price.price THEN 1 
-        WHEN current_price.price > previous_price.price AND current_price.sale_price IS NOT NULL THEN 2 
-        ELSE NULL END gouge_type
- FROM cte current_price
- JOIN cte previous_price 
- ON current_price.booz_id = previous_price.booz_id AND previous_price.row_num = 2 
- WHERE current_price.row_num = 1""")
+    (SELECT 
+            b.booz_id,
+            b.booz_name,
+            b.link,
+            bs.price,
+            bs.sale_price,
+            bs.run_id,
+            ROW_NUMBER() OVER (PARTITION BY bs.booz_id ORDER BY bs.scrape_date DESC) AS row_num
+        FROM booz_scraped bs
+        JOIN booz  b
+        ON bs.booz_id = b.booz_id
+     	JOIN (select * from run order by run_id desc LIMIT 2) r 
+     	on bs.run_id = r.run_id 
+     )
+    SELECT
+        current_price.booz_id,
+        current_price.booz_name,
+        current_price.link,
+        current_price.price c_price,
+        current_price.sale_price c_sale_price,
+        CAST(100-(current_price.sale_price/current_price.price * 100) AS INT) c_discount,
+        previous_price.price p_price,
+        previous_price.sale_price p_sale_price,
+        CAST(100 - (previous_price.sale_price/previous_price.price *100) AS INT) p_discount,
+        CASE WHEN current_price.sale_price >= previous_price.price THEN 1 
+            WHEN current_price.price > previous_price.price AND current_price.sale_price IS NOT NULL THEN 2 
+            ELSE NULL END gouge_type
+    FROM cte current_price
+    JOIN cte previous_price 
+    ON current_price.booz_id = previous_price.booz_id AND previous_price.row_num = 2 
+    WHERE current_price.row_num = 1""")
     new_or_changed_prices = cursor.fetchall()
     formatted_new_or_changed_prices = [f'''<a href="{row["link"]}">{row["booz_name"]}</a> ({row["booz_id"]})<br>'''
                           + (f'''Currently <s>${row["c_price"]}</s> <b>${row["c_sale_price"]} {row["c_discount"]}%</b> off!''' if row["c_sale_price"] is not None else 
